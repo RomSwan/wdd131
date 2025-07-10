722d53f3f3ffe264f2ddd18365c9bf3627e6ab4b
RomSwan <legion.of.swans@gmail.com>
2025-07-10 16:14:40 +0200

    finished bom activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,28 @@
-const input = document.querySelector("#favechap");
+const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener("click", function () 
+{
+    if (input.value.trim() !== "") 
+    {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
 
-li.textContent = input.value; // Populate the li element variable's textContent or innerHTML with the input value.
-deleteButton.textContent = "❌"; // Populate the button textContent with a ❌.
-
-li.append(deleteButton); // Append the li element variable with the delete button.
-list.append(li); // Append the li element variable to the unordered list in your HTML.
+        li.textContent = input.value; // Populate the li element variable's textContent or innerHTML with the input value.
+        deleteButton.textContent = "❌"; // Populate the button textContent with a ❌.
+    
+        deleteButton.addEventListener("click", function()
+        {
+            list.removeChild(li);
+            input.focus();
+        });
+    
+        li.append(deleteButton); // Append the li element variable with the delete button.
+        list.append(li); // Append the li element variable to the unordered list in your HTML.
+    
+        input.value = "";
+    }
+    
+    input.focus()
+});
