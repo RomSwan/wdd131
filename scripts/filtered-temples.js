6fa2b204dccc37616808cc88997bec6afb0f1ab5
RomSwan <legion.of.swans@gmail.com>
2025-07-16 10:12:33 +0200

    added 3 temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -10,7 +10,7 @@ hamButton.addEventListener('click', () => {
 
 // Temples
 const temples = [
-  { // #1
+  { // #01
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
@@ -18,7 +18,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
-  { // #2
+  { // #02
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
@@ -26,7 +26,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
   },
-  { // #3
+  { // #03
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
@@ -34,7 +34,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
-  { // #4
+  { // #04
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
@@ -42,7 +42,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
   },
-  { // #5
+  { // #05
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
@@ -50,7 +50,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
   },
-  { // #6
+  { // #06
     templeName: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
@@ -58,7 +58,7 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
   },
-  { // #7
+  { // #07
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
@@ -66,5 +66,28 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
-  // Add more temple objects here...
+  { // #08
+    templeName: "Cardston Alberta",
+    location: "Alberta, Canada",
+    dedicated: "1923, August, 29",
+    area: 13287,
+    imageURL:
+    "https://churchofjesuschristtemples.org/assets/img/temples/cardston-alberta-temple/cardston-alberta-temple-13287-main.jpg"
+  },
+  { // #09
+    templeName: "Frankfurt Germany",
+    location: "Frankfurt, Germany",
+    dedicated: "1987, August, 30",
+    area: 38924,
+    imageURL:
+    "https://churchofjesuschristtemples.org/assets/img/temples/frankfurt-germany-temple/frankfurt-germany-temple-38924-main.jpg"
+  },
+  { // #10
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2019, May, 4",
+    area: 26495,
+    imageURL:
+    "https://churchofjesuschristtemples.org/assets/img/temples/yigo-guam-temple/yigo-guam-temple-26495-main.jpg"
+  }
 ];
