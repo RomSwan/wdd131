df0b4c3d62b176b8ee82a767166bb0e28ace1eb6
RomSwan <legion.of.swans@gmail.com>
2025-08-05 10:17:10 +0200

    hamburger nav bar

---
+++ week06-project/scripts/warsaw-guitars.js
@@ -0,0 +1,8 @@
+// HAMBURGER MENU BUTTON
+const hamButton = document.querySelector('#menu');
+const nav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
