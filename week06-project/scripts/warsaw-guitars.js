2da5f6a03be7f960f5e3deeb7b8820b20b451ce1
RomSwan <legion.of.swans@gmail.com>
2025-08-07 08:53:17 +0200

    removed getdates.js

--- week06-project/scripts/warsaw-guitars.js
+++ week06-project/scripts/warsaw-guitars.js
@@ -6,3 +6,11 @@ hamButton.addEventListener('click', () => {
   nav.classList.toggle('open');
   hamButton.classList.toggle('open');
 });
+
+//Get current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+//Last modified
+const lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last modified: ${lastModified}`;
